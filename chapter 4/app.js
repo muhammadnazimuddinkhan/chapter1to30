c912b935d23ffb9ae2e0e0d4b44798c164600dac
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2026-01-20 06:40:47 +0500

    chapter4

---
+++ chapter 4/app.js
@@ -0,0 +1,27 @@
+//  Declare 3 variables in one statement
+// var a =10 , b= 20,c=30
+
+//  Declare 5 legal & 5 illegal variable names
+// var names;
+// var _age;
+// var user1;
+// var $price;
+// var firstName;
+//  //illegal
+//  var 1names;
+//  var full names;
+//  var @age;
+//  var first-names;
+//  var var;
+
+console.log("<h1>Rules for naming JS variables</h1>");
+
+console.log("Variable names can only contain letters, numbers, $ and _. ");
+console.log("For example: $my_1stVariable<br><br>");
+
+console.log("Variables must begin with a letter, $ or _. ");
+console.log("For example: $name, _name or name<br><br>");
+
+console.log("Variable names are case sensitive<br><br>");
+
+console.log("Variable names should not be JS keywords");

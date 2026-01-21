40f167b247f4cafeba057df66fee19a681f45de8
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2026-01-21 09:43:44 +0500

    chapter5

---
+++ chapter 5/app.js
@@ -0,0 +1,144 @@
+// Write a program that take two numbers & add them in a 
+// new variable. Show the result in your browser.
+// var sum = 3+5;
+// console.log(sum)
+
+//  Repeat task1 for subtraction, multiplication, division & 
+// modulus. 
+// var num1=5-3
+// var num2 = 5*3
+// var num3 = 4/2
+// var num4= 5%3
+// console.log(num1,num2,num3,num4)
+
+// . Do the following using JS Mathematic Expressions 
+// a. Declare a variable. 
+// b. Show the value of variable in your browser like “Value 
+// after variable declaration is: ??”. 
+// c. Initialize the variable with some number. 
+// d. Show the value of variable in your browser like “Initial 
+// value: 5”. 
+// e. Increment the variable. 
+// f. Show the value of variable in your browser like “Value 
+// after increment is: 6”. 
+// g. Add 7 to the variable. 
+// h. Show the value of variable in your browser like “Value 
+// after addition is: 13”. 
+// i. Decrement the variable. 
+// j. Show the value of variable in your browser like “Value 
+// after decrement is: 12”. 
+// k. Show the remainder after dividing the variable’s value 
+// by 3.  
+// l. Output : “The remainder is : 0”.
+// var num;
+// console.log("value after variable declaration is:" ,num)
+// num= 5;
+// console.log("initial value:",num)
+// num++
+// console.log("value after increment is :",num)
+// num= num+7
+// console.log("value after addition is :",num)
+// num--
+// console.log("value after decrement is :",num)
+// var remainder = 12%3
+// console.log("the remainder is:",remainder)
+
+//  Cost of one movie ticket is 600 PKR. Write a script to 
+// store 
+// ticket price in a variable & calculate the cost of buying 5 
+// tickets 
+// to a movie. Example output:total cost to by 5 tickets to a movie is PKR3000
+// var ticketPrice =600;
+// var totalcost= 600*5;
+// console.log("Total cost to by 5 ticket to a movie is:",totalcost)
+
+// var num = 2;
+
+//   // Display table heading
+//   console.log("<h2>Multiplication Table of " + num + "</h2>");
+
+//   // Generate table
+//   for (var i = 1; i <= 10; i++) {
+//     console.log(num + " x " + i + " = " + (num * i)  );
+//   }
+
+// var item1=650
+// var item2=100
+// var shipchg =100
+// var totalcost=item1*3 + item2*7 + shipchg
+// console.log("price of item1:",item1,"purchase",(item1*3))
+// console.log("price of item2:",item2 ,"purchased",(item2*7))
+// console.log("total amount of your purchaed is :",totalcost)
+
+//  Write a program to implement checkout process of a 
+// shopping cart system for an e-commerce website. Store 
+// the following in variables 
+// a. Price of item 1 
+// b. Price of item 2 
+// c. Ordered quantity of item 1 
+// d. Ordered Quantity of item 2 
+// e. Shipping charges 
+// Compute the total cost & show the receipt in your browser.
+
+// var priceItem1 = 650;
+//     var priceItem2 = 100;
+
+//     // Quantities
+//     var qtyItem1 = 3;
+//     var qtyItem2 = 7;
+
+//     // Shipping charges
+//     var shippingCharges = 100;
+
+//     // Calculations
+//     var totalCost = (priceItem1 * qtyItem1) + (priceItem2 * qtyItem2) + shippingCharges;
+
+//     // Display receipt
+//     console.log("<h2>Shopping Cart</h2>");
+//     console.log("Price of item 1 is " + priceItem1 + "<br>");
+//     console.log("Quantity of item 1 is " + qtyItem1 + "<br>");
+//     console.log("Price of item 2 is " + priceItem2 + "<br>");
+//     console.log("Quantity of item 2 is " + qtyItem2 + "<br><br>");
+//     console.log("Shipping Charges " + shippingCharges + "<br><br>");
+//     console.log("<b>Total cost of your order is " + totalCost + "</b>");
+
+// var totalMarks= 1000
+// var marksObt= 800
+// var percentage= (marksObt/totalMarks)*100
+// console.log(percentage,"%")
+
+// . Assume we have 10 US dollars & 25 Saudi Riyals. Write a 
+// script to convert the total currency to Pakistani Rupees. 
+// Perform all calculations in a single expression. 
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee 
+// and 1 Saudi Riyal = 28 Pakistani Rupee)
+// var totalPkr =(10*104.80) + (25*28);
+// console.log("total currency in pkr:" ,totalPkr)
+
+//  Write a program to initialize a variable with some 
+// number and do arithmetic in following sequence: 
+// a. Add 5 
+// b. Multiply by 10 
+// c. Divide the result by 2 
+// Perform all calculations in a single expression 
+// var num = 5
+// var totalNum=(((num+5)*10)/2)
+// console.log(totalNum)
+
+//  The Age Calculator: Forgot how old someone is? 
+// Calculate it! 
+// a. Store the current year in a variable. 
+// b. Store their birth year in a variable. 
+// c. Calculate their 2 possible ages based on the stored 
+// values. 
+// var currentYear =2026;
+// var birthYear =1974;
+// var age1= currentYear-birthYear
+// var age2=age1-1
+// console.log("two possible ages ",age1,"or",age2)
+
+// var currentAge= 50
+// var posibAage=80
+// var snackPday=2
+// var totalSnackReq = ((posibAage-currentAge)*365)*2
+// console.log(totalSnackReq)

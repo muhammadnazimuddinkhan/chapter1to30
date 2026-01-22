ddd2e055cb05b128ec2aa4148576cb088d757109
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2026-01-22 09:22:24 +0500

    chapter 6 to9

---
+++ chapter 6 to 9/app.js
@@ -0,0 +1,96 @@
+//  Write a program to take a number in a variable, do the 
+// required arithmetic to display the following result in your 
+// browser: 
+
+// var a = 10;
+
+// console.log("Result:");
+// console.log("The value of a is: " + a);
+// console.log(".................................");
+
+// console.log("The value of ++a is: " + (++a));
+// console.log("Now the value of a is: " + a);
+
+// console.log("");
+
+// console.log("The value of a++ is: " + (a++));
+// console.log("Now the value of a is: " + a);
+
+// console.log("");
+
+// console.log("The value of --a is: " + (--a));
+// console.log("Now the value of a is: " + a);
+
+// console.log("");
+
+// console.log("The value of a-- is: " + (a--));
+// console.log("Now the value of a is: " + a);
+
+// 2. What will be the output in variables a, b & result after 
+// execution of the following script: 
+// var a = 2, b = 1; 
+//  var result = --a - --b + ++b + b--; 
+//  Explain the output at each stage: --a; --a - --b; --a - --b + ++b; --a - --b + ++b + b--; 
+//  console.log(--a)// 1
+//  console.log(--a - --b)// 1
+//  console.log(--a - --b + ++b)//2
+// console.log(--a, - --b, + ++b, + b--)//2
+
+//  Write a program that takes input a name from user & 
+// greet the user.
+// var firstName = prompt("please enter your first name:")
+// var lastName = prompt("please enter your last name:")
+
+// alert("Hello " + firstName + lastName + " welcome!")
+
+// Write a program to take input a number from user & 
+// display it’s multiplication table on your browser. If user 
+// does not enter a new number, multiplication table of 5 
+// should be displayed by default.
+// var number=prompt("please enter a number for table:")
+// if(number==="" || number==="null"){
+//     number=5
+// }
+// for (var i = 1;i<=10; i++){
+//     console.log(number,"x",i,"=",(number*i))
+// }
+
+//  Take 
+// a) Take three subjects name from user and store them in 3 
+// different variables. 
+// b) Total marks for each subject is 100, store it in another 
+// variable. 
+// c) Take obtained marks for first subject from user and 
+// stored it in different variable
+// d) Take obtained marks for remaining 2 subjects from user 
+// and store them in variables. 
+// e) Now calculate total marks and percentage and show the 
+// result in browser like this.(Hint: user table)
+  // a) Take three subject names
+    // var subject1 = prompt("Enter first subject name:");
+    // var subject2 = prompt("Enter second subject name:");
+    // var subject3 = prompt("Enter third subject name:");
+
+    // // b) Total marks for each subject
+    // var totalMarksPerSubject = 100;
+
+    // // c & d) Take obtained marks
+    // var marks1 = Number(prompt("Enter obtained marks for " + subject1));
+    // var marks2 = Number(prompt("Enter obtained marks for " + subject2));
+    // var marks3 = Number(prompt("Enter obtained marks for " + subject3));
+
+    // // e) Calculate totals and percentage
+    // var totalObtainedMarks = marks1 + marks2 + marks3;
+    // var totalMarks = totalMarksPerSubject * 3;
+    // var percentage = (totalObtainedMarks / totalMarks) * 100;
+
+    // // Display result in console
+    // console.log("Subject\tTotal Marks\tObtained Marks");
+    // console.log(subject1 + "\t100\t\t" + marks1);
+    // console.log(subject2 + "\t100\t\t" + marks2);
+    // console.log(subject3 + "\t100\t\t" + marks3);
+    // console.log("-------------------------------");
+    // console.log("Total\t" + totalMarks + "\t\t" + totalObtainedMarks);
+    // console.log("Percentage: " + percentage + "%");
+
+    

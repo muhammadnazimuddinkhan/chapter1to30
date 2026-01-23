12ab70619fa57d886ce64d707059e696a19f300f
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2026-01-23 09:11:52 +0500

    chapter1213

---
+++ chapter 12 to 13/app.js
@@ -0,0 +1,89 @@
+//  Write a program that takes a character (number or string) 
+// in a variable & checks whether the given input is a 
+// number, uppercase letter or lower case letter. (Hint: ASCII 
+// codes:- A=65, Z=90, a=97, z=122).
+// var ch = prompt("Enter a character:");
+//     var ascii = ch.charCodeAt(0);
+
+//     if (ascii >= 48 && ascii <= 57) {
+//         document.write("The input is a Number.");
+//     }
+//     else if (ascii >= 65 && ascii <= 90) {
+//         document.write("The input is an Uppercase Letter.");
+//     }
+//     else if (ascii >= 97 && ascii <= 122) {
+//         document.write("The input is a Lowercase Letter.");
+//     }
+//     else {
+//         document.write("Invalid input.");
+//     }
+
+//  Write a JavaScript program that accept two integers and 
+// display the larger. Also show if the two integers are equal.
+//    var num1 = parseInt(prompt("Enter first integer:"));
+//     var num2 = parseInt(prompt("Enter second integer:"));
+
+//     if (num1 > num2) {
+//         console.log("The larger number is: " + num1);
+//     }
+//     else if (num2 > num1) {
+//         console.log("The larger number is: " + num2);
+//     }
+//     else {
+//         console.log("Both numbers are equal.");
+//     }
+// var num = parseInt(prompt("Enter a number:"));
+
+// if (num > 0) {
+//     console.log("The number is Positive.");
+// }
+// else if (num < 0) {
+//     console.log("The number is Negative.");
+// }
+// else {
+//     console.log("The number is Zero.");
+// }
+// var ch = prompt("Enter a single character:");
+// ch = ch.toLowerCase();
+
+// if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+//  Write a program that 
+// a. Store correct password in a JS variable. 
+// b.  Asks user to enter his/her password 
+// c.  Validate the two passwords: 
+// i. 
+// Check if user has entered password. If not, then 
+// give message “ Please enter your password” 
+// ii. 
+// Check if both passwords are same. If they are 
+// same, show message “Correct! The password you 
+// entered matches the original password”. Show 
+// “Incorrect password” otherwise.
+// var correctPassword = "js123";
+
+// var userPassword = prompt("Enter your password:");
+
+// if (userPassword === "" || userPassword === null) {
+//     console.log("Please enter your password");
+// }
+// else if (userPassword === correctPassword) {
+//     console.log("Correct! The password you entered matches the original password");
+// }
+// else {
+//     console.log("Incorrect password");
+// }
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+
+// console.log(greeting);
+
